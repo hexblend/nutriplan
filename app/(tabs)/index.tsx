552b78eba796cbe4ebb44d6c2fd1956c7b4d5519
Vlad Bibire <vladbbr1@gmail.com>
2025-03-04 21:05:08 +0200

    native wind works

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,18 @@
-import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { verifyInstallation } from 'nativewind';
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+	verifyInstallation();
+	return (
+		<View className="flex-1 items-center justify-center">
+			<Text className="text-xl font-bold">Tab One</Text>
+			<View
+				className="h-1 w-[80%] bg-gray-200 my-4"
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<EditScreenInfo path="app/(tabs)/index.tsx" />
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
