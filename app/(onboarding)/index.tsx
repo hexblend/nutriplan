f90454d90076ff0277ddef7a770501871fd3c3b6
Vlad Bibire <vladbbr1@gmail.com>
2025-03-05 21:01:14 +0200

    Phone input

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,20 +1,25 @@
 import { Button } from '@/components/ui/button';
-import { ControlledInput } from '@/components/ui/form/ControlledInput';
 import { Text } from '@/components/ui/text';
-import { supabase } from '@/lib/supabase/client';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { View } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
+import { Alert, View } from 'react-native';
+import PhoneInput, {
+  ICountry,
+  isValidPhoneNumber,
+} from 'react-native-international-phone-number';
 import * as z from 'zod';
 
 const formSchema = z.object({
-  email: z.string().email({ message: 'Please enter a valid email address' }),
+  phone: z.string(),
 });
 type FormValues = z.infer<typeof formSchema>;
 
 export default function OnboardingMainScreen() {
+  const [selectedCountry, setSelectedCountry] = useState<undefined | ICountry>(
+    undefined
+  );
   const router = useRouter();
 
   const [, setLoading] = useState(false);
@@ -22,39 +27,62 @@ export default function OnboardingMainScreen() {
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    // formState: { errors },
   } = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: '',
+      phone: '',
     },
   });
 
   const onSubmit = async (data: FormValues) => {
-    setLoading(true);
-    const { error } = await supabase.auth.signUp({
-      email: data.email,
-      password: 'asdasdasd',
-    });
-    if (error) {
-      console.log(error);
-      return setLoading(false);
+    const { phone } = data;
+    const phoneNumber = `${selectedCountry?.callingCode} ${phone}`;
+    const isValid = isValidPhoneNumber(phone, selectedCountry as ICountry);
+    if (!isValid) {
+      return Alert.alert(
+        'Invalid Phone Number',
+        'Please enter a valid phone number.'
+      );
     }
+    console.log(phoneNumber);
+    setLoading(true);
+    // let { data: userData, error } = await supabase.auth.signInWithOtp({
+    //   phone: '+40799156290',
+    // });
+    // let { data: userData, error } = await supabase.auth.verifyOtp({
+    //   phone: '+40799156290',
+    //   token: '137717',
+    //   type: 'sms',
+    // });
+    // if (error) {
+    //   console.log('ERROR', error);
+    //   return setLoading(false);
+    // }
+    // console.log('SUCCESS', userData);
     setLoading(false);
     router.push('/(onboarding)/otp');
   };
 
   return (
     <View className="flex-1 items-center justify-center px-4">
-      <ControlledInput
+      <Controller
+        name="phone"
         control={control}
-        name="email"
-        label="Email"
-        placeholder="Enter your email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-        error={errors.email?.message}
+        render={({ field: { onChange, value } }) => (
+          <PhoneInput
+            value={value}
+            onChangePhoneNumber={onChange}
+            selectedCountry={selectedCountry}
+            onChangeSelectedCountry={(country) => setSelectedCountry(country)}
+            theme="dark"
+            modalHeight="80%"
+            popularCountries={['RO', 'US', 'GB']}
+            allowZeroAfterCallingCode={false}
+            defaultCountry="RO"
+            language="ro"
+          />
+        )}
       />
 
       <Button
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,19 @@
+import { useColorScheme } from '@/components/ui/useColorScheme';
+import { NAV_THEME } from '@/lib/constants';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Theme, ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
 import '../assets/styles/global.css';
-import { NAV_THEME } from '@/lib/constants';
-import React from 'react';
-import { useColorScheme } from '@/components/ui/useColorScheme';
-import { StatusBar } from 'expo-status-bar';
 
 const LIGHT_THEME: Theme = {
   ...DefaultTheme,
@@ -67,7 +71,8 @@ export default function RootLayout() {
     <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
       <StatusBar style="light" />
       <Stack>
-        <Stack.Screen redirect name="index" /> {/* Workaround to go to onboarding */}
+        <Stack.Screen redirect name="index" />{' '}
+        {/* Workaround to go to onboarding */}
         <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         <Stack.Screen name="(main)" options={{ headerShown: false }} />
