4e1afd8f674212cf354889845052d24fb71ec8f3
Vlad Bibire <vladbbr1@gmail.com>
2025-03-27 10:32:57 +0200

    Paywall looking great

--- app/auth/(onboarding)/paywall.tsx
+++ app/auth/(onboarding)/paywall.tsx
@@ -10,9 +10,6 @@ import { Image, View } from 'react-native';
 
 export default function PaywallScreen() {
   const onSubmit = () => {};
-  const boldWords =
-    appLanguage === 'en' ? ['goals', 'YOUR'] : ['obiectivele', 'tău'];
-
   return (
     <PageWrapper
       footer={
@@ -23,22 +20,69 @@ export default function PaywallScreen() {
         </PageFooter>
       }
     >
-      <View className="mt-2 items-center">
+      <View className="items-center">
         <Image
           source={{ uri: `${bucketUrl}/paywall.png` }}
           style={{ width: 195.5, height: 166.5 }} // Original size: 391 x 333 / 2
         />
         <AnimatedTitle
-          title={t.t('auth.paywallTitle')}
+          title={
+            appLanguage === 'en'
+              ? 'Say hello to your personalized weekly meal plans'
+              : 'Salută planul tău alimentar personalizat săptămlânal'
+          }
           delay={100}
           centered
           className="mt-6"
         />
-
         <PaywallBenefit
           className="mt-4"
-          text={t.t('auth.paywallBenefit')}
-          boldWords={boldWords}
+          text={
+            appLanguage === 'en'
+              ? 'Reach your goals based on YOUR lifestyle'
+              : 'Atinge-ți obiectivele bazate pe stilul tău de viață'
+          }
+          boldWords={
+            appLanguage === 'en' ? ['goals', 'YOUR'] : ['obiectivele', 'tău']
+          }
+        />
+        <PaywallBenefit
+          className="mt-4"
+          text={
+            appLanguage === 'en'
+              ? 'You get weekly meal plans, shopping lists and cooking instructions'
+              : 'Primești planuri alimentare, liste de cumpărături și instrucțiuni de gătire'
+          }
+          boldWords={
+            appLanguage === 'en'
+              ? ['weekly meal plans,', 'shopping lists', 'cooking instructions']
+              : [
+                  'planuri alimentare',
+                  'liste de cumpărături',
+                  'instrucțiuni de gătire',
+                ]
+          }
+        />
+        <PaywallBenefit
+          className="mt-4"
+          text={
+            appLanguage === 'en'
+              ? "You don't worry about calories, we calculate each portion for you."
+              : 'Nu te preocupi de calorii, calculăm fiecare porție pentru tine.'
+          }
+          boldWords={
+            appLanguage === 'en' ? ["don't worry"] : ['Nu te preocupi']
+          }
+        />
+        <AnimatedTitle
+          title={
+            appLanguage === 'en'
+              ? 'Join hundreds of users who maintain a healthy lifestyle'
+              : 'Alătură-te sutelor de utilizatori ce mențin o viață sănătoasă'
+          }
+          delay={1500}
+          centered
+          className="mt-8"
         />
       </View>
     </PageWrapper>
