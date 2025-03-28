a5115d1815aa1b071afe6232bda8b911e56a345b
Vlad Bibire <vladbbr1@gmail.com>
2025-03-28 11:34:47 +0200

    Great version

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,39 +1,25 @@
+import TabBarBackground from '@/components/layout/TabBar/TabBarBackground';
 import TabBarIcon from '@/components/layout/TabBar/TabBarIcon';
 import TabBarLabel from '@/components/layout/TabBar/TabBarLabel';
 import { colors } from '@/lib/constants';
-import { Tabs, usePathname } from 'expo-router';
-import React, { useEffect, useRef } from 'react';
-import { Animated, Dimensions } from 'react-native';
+import { Tabs } from 'expo-router';
+import React, { useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function MainLayout() {
   const instets = useSafeAreaInsets();
-  const tabBarWidth = Dimensions.get('window').width - 24; // Accounting for left/right margins
-  const tabWidth = tabBarWidth / 3; // Since we have 3 tabs
-  const animatedValue = useRef(new Animated.Value(0)).current;
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const getIndex = () => {
-      if (pathname === '/') return 0;
-      if (pathname === '/profile') return 1;
-      if (pathname === '/feedback') return 2;
-      return 0;
-    };
-
-    Animated.spring(animatedValue, {
-      toValue: getIndex(),
-      useNativeDriver: true,
-      tension: 50,
-      friction: 7,
-    }).start();
-  }, [pathname]);
+  const [textWidths, setTextWidths] = useState<number[]>([
+    'Meal Plans'.length * 8,
+    'Profile'.length * 8,
+    'Feedback'.length * 8,
+  ]);
 
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarStyle: {
+          backgroundColor: colors.primary[700],
           marginBottom: instets.bottom,
           marginLeft: 12,
           marginRight: 12,
@@ -42,38 +28,21 @@ export default function MainLayout() {
           height: 70,
           paddingBottom: 0,
           borderWidth: 1,
-          borderBottomWidth: 3,
+          borderBottomWidth: 1,
           borderTopWidth: 1,
+          borderColor: colors.primary[800],
           position: 'relative',
         },
         tabBarActiveTintColor: '#FFFFFF',
         tabBarInactiveTintColor: '#9CA3AF',
         tabBarIconStyle: {
-          marginTop: 6,
+          marginTop: 7,
           marginBottom: 1.5,
         },
         tabBarLabel: ({ focused, children }) => (
           <TabBarLabel focused={focused}>{children}</TabBarLabel>
         ),
-        tabBarBackground: () => (
-          <Animated.View
-            style={{
-              position: 'absolute',
-              bottom: 0,
-              width: tabWidth,
-              height: 2,
-              backgroundColor: colors.primary[400],
-              transform: [
-                {
-                  translateX: animatedValue.interpolate({
-                    inputRange: [0, 1, 2],
-                    outputRange: [0, tabWidth, tabWidth * 2],
-                  }),
-                },
-              ],
-            }}
-          />
-        ),
+        tabBarBackground: () => <TabBarBackground textWidths={textWidths} />,
       }}
     >
       <Tabs.Screen
@@ -84,23 +53,13 @@ export default function MainLayout() {
           tabBarIcon: ({ focused }) => (
             <TabBarIcon name="logo" color="#FFFFFF" focused={focused} />
           ),
-          //   headerRight: () => (
-          //     <Link href="/modal">
-          //       <Pressable>
-          //         {({ pressed }) => (
-          //           <MaterialCommunityIcons
-          //             name="info-circle"
-          //             size={25}
-          //             color={'#FFF'}
-          //             style={{
-          //               marginRight: 15,
-          //               opacity: pressed ? 0.5 : 1,
-          //             }}
-          //           />
-          //         )}
-          //       </Pressable>
-          //     </Link>
-          //   ),
+        }}
+        listeners={{
+          tabPress: () => {
+            const text = 'Meal Plans';
+            const width = text.length * 8;
+            setTextWidths((prev) => [width, prev[1], prev[2]]);
+          },
         }}
       />
       <Tabs.Screen
@@ -115,6 +74,13 @@ export default function MainLayout() {
             />
           ),
         }}
+        listeners={{
+          tabPress: () => {
+            const text = 'Profile';
+            const width = text.length * 8;
+            setTextWidths((prev) => [prev[0], width, prev[2]]);
+          },
+        }}
       />
       <Tabs.Screen
         name="feedback"
@@ -127,6 +93,13 @@ export default function MainLayout() {
               focused={focused}
             />
           ),
+        }}
+        listeners={{
+          tabPress: () => {
+            const text = 'Feedback';
+            const width = text.length * 8;
+            setTextWidths((prev) => [prev[0], prev[1], width]);
+          },
         }}
       />
     </Tabs>
---
+++ components/layout/TabBar/TabBarBackground.tsx
@@ -0,0 +1,70 @@
+import { colors } from '@/lib/constants';
+import { usePathname } from 'expo-router';
+import React, { useEffect, useRef } from 'react';
+import { Animated, Dimensions, View } from 'react-native';
+
+interface TabBarBackgroundProps {
+  textWidths: number[];
+}
+
+export default function TabBarBackground({
+  textWidths,
+}: TabBarBackgroundProps) {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const pathname = usePathname();
+  const tabBarWidth = Dimensions.get('window').width - 24;
+  const tabWidth = tabBarWidth / 3;
+
+  useEffect(() => {
+    const getIndex = () => {
+      if (pathname === '/') return 0;
+      if (pathname === '/profile') return 1;
+      if (pathname === '/feedback') return 2;
+      return 0;
+    };
+
+    Animated.spring(animatedValue, {
+      toValue: getIndex(),
+      useNativeDriver: true,
+      tension: 50,
+      friction: 7,
+    }).start();
+  }, [pathname]);
+
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        left: '11%',
+        right: 12,
+        height: 3,
+      }}
+    >
+      <Animated.View
+        style={{
+          position: 'absolute',
+          bottom: -1,
+          height: 2.5,
+          backgroundColor: colors.primary[400],
+          borderRadius: 1,
+          transform: [
+            {
+              translateX: animatedValue.interpolate({
+                inputRange: [0, 1, 2],
+                outputRange: [0, tabWidth, tabWidth * 2],
+              }),
+            },
+            {
+              scaleX: animatedValue.interpolate({
+                inputRange: [0, 1, 2],
+                outputRange: textWidths.map((width) => width / 40),
+              }),
+            },
+          ],
+          width: 40,
+        }}
+      />
+    </View>
+  );
+}
