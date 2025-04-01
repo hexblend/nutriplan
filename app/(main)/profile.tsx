92d19d93d66b581ca61663a1ca259649fed2a7a2
Vlad Bibire <vladbbr1@gmail.com>
2025-04-01 16:26:02 +0300

    Avatar on profile

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,17 +1,24 @@
 import PageWrapper from '@/components/layout/PageWrapper';
 import Card from '@/components/main/Card';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Text } from '@/components/ui/text';
 import { View } from 'react-native';
 
 export default function ProfileScreen() {
   return (
     <PageWrapper>
-      <View className="flex-1 bg-background">
-        <Text className="mb-6 text-2xl font-bold text-foreground">Profile</Text>
-        <Card>
+      <Text className="mb-6 text-2xl font-bold text-foreground">Profile</Text>
+
+      <Card className="flex-row items-start justify-between">
+        <View>
           <Text>Hello</Text>
-        </Card>
-      </View>
+        </View>
+        <Avatar alt="Avatar" className="h-20 w-20">
+          <AvatarFallback>
+            <Text className="text-2xl font-bold">ZN</Text>
+          </AvatarFallback>
+        </Avatar>
+      </Card>
     </PageWrapper>
   );
 }
