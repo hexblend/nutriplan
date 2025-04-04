ab3cea191574cdb442b30de0946542812827dc73
Vlad Bibire <vladbbr1@gmail.com>
2025-04-04 20:32:40 +0300

    basid edit screens

---
+++ app/profile/edit-age.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+export default function EditAgeScreen() {
+  return (
+    <View>
+      <Text>Edit Age</Text>
+    </View>
+  );
+}
---
+++ app/profile/edit-height.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+export default function EditHeightScreen() {
+  return (
+    <View>
+      <Text>Edit Age</Text>
+    </View>
+  );
+}
