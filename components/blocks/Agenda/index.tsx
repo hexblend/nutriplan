d7f9b25c585335eda0e247b75745ce3bf24190b0
Vlad Bibire <vladbbr1@gmail.com>
2025-04-08 13:42:01 +0300

    Ux somewhat good

--- components/blocks/Agenda/index.tsx
+++ components/blocks/Agenda/index.tsx
@@ -1,13 +1,17 @@
 import { colors } from '@/lib/constants';
+import { format } from 'date-fns';
 import React from 'react';
 import CalendarStrip from 'react-native-calendar-strip';
 
 export default function Agenda() {
+  // Format today's date to compare with other dates
+  const today = format(new Date(), 'yyyy-MM-dd');
+
   return (
     <CalendarStrip
       numDaysInWeek={7}
-      //   scrollable
-      //   scrollerPaging
+      scrollable
+      scrollerPaging
       //   calendarHeaderFormat="MMMM"
       calendarColor={colors.primary[700]}
       //  Header
@@ -15,10 +19,15 @@ export default function Agenda() {
       calendarHeaderStyle={{
         color: 'white',
       }}
-      calendarHeaderContainerStyle={{ marginTop: 8, marginBottom: 8 }}
+      calendarHeaderContainerStyle={{ marginTop: 8, marginBottom: 13 }}
       // Date
       dateNumberStyle={{ color: 'white', fontSize: 14 }}
       dateNameStyle={{ color: 'white', fontSize: 10 }}
+      // Blacklist all days except today
+      datesBlacklist={(date) => {
+        const dateString = date.format('YYYY-MM-DD');
+        return dateString !== today;
+      }}
       // Icon
       iconContainer={{ flex: 0.1, marginBottom: 2 }}
       /* eslint-disable */
@@ -29,16 +38,18 @@ export default function Agenda() {
       calendarAnimation={{ type: 'sequence', duration: 50 }}
       // Selected Date
       selectedDate={new Date()}
-      highlightDateNumberStyle={{ color: 'white', fontSize: 14 }}
-      highlightDateNameStyle={{ color: 'white', fontSize: 10 }}
       dayContainerStyle={{
-        borderRadius: 2,
+        borderRadius: 4,
       }}
       highlightDateContainerStyle={{
         borderBottomWidth: 2,
         borderColor: colors.primary[300],
         backgroundColor: colors.primary[800],
       }}
+      highlightDateNumberStyle={{ color: 'white', fontSize: 14 }}
+      highlightDateNameStyle={{ color: 'white', fontSize: 10 }}
+      disabledDateNumberStyle={{ color: 'white', fontSize: 14 }}
+      disabledDateNameStyle={{ color: 'white', fontSize: 10 }}
       //   daySelectionAnimation={{
       //     type: 'background',
       //     duration: 200,
