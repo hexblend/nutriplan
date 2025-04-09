6fed9a4bf617d4d7cd909cb27b4bb7c5cf0ae56d
Vlad Bibire <vladbbr1@gmail.com>
2025-04-09 09:42:33 +0300

    Create screen

--- app/plans/create.tsx
+++ app/plans/create.tsx
@@ -9,7 +9,7 @@ import { calculateWeeksToGoal, displayWeight } from '@/lib/utils';
 import { useSession } from '@/providers/SessionProvider';
 import { View } from 'react-native';
 
-export default function StartScreen() {
+export default function CreateScreen() {
   const { currentClient, currentProfile } = useSession();
   const weeksToGoal = calculateWeeksToGoal(currentClient);
   const weightGoal = displayWeight(
