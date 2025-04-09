22bda18db735a7eef0e1fa49d112e02cb068a102
Vlad Bibire <vladbbr1@gmail.com>
2025-04-09 09:41:55 +0300

    Rename create screen

--- app/plans/_layout.tsx
+++ app/plans/_layout.tsx
@@ -24,7 +24,7 @@ export default function PlansLayout() {
         }}
       >
         <Stack.Screen
-          name="start"
+          name="create"
           options={{
             title: ' ',
             headerStyle: {
